81d2105d96c65f8287ae613a4a6c088d8795c652
slb <Christian.Schellenberger@mbbm-vas.com>
2025-09-30 09:43:16 +0200

    beta uses now v2 of gamma

--- beta.go
+++ beta.go
@@ -2,9 +2,9 @@ package mvslibbeta
 
 import (
 	"fmt"
-	"github.com/mbbm-slb/mvs_lib_gamma"
+	"github.com/mbbm-slb/mvs_lib_gamma/v2"
 )
 
 func Beta() string {
-	return fmt.Sprintf("Beta forwarding: %s", mvslibgamma.Gamma())
+	return fmt.Sprintf("Beta forwarding: %s", mvslibgamma.Gamma("beta uses v2 of gamma", "yay!"))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mbbm-slb/mvs_lib_beta
 
 go 1.25.1
 
-require github.com/mbbm-slb/mvs_lib_gamma v0.3.0
+require github.com/mbbm-slb/mvs_lib_gamma/v2 v2.0.1
